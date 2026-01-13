2391156d31c71d4c4769774bd87f6eaf8c38a119
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-01-13 14:01:19 +0800

    Updating

--- Algorithm/Easy/Question_1.py
+++ Algorithm/Easy/Question_1.py
@@ -11,4 +11,18 @@ print(nums)  # Output: [1, 2, 3, 3, 4]
 def remove_duplicates(nums):
     if not nums:
         return 0 
-    
+    elif len(nums) == 1:
+        return 1
+    else:
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+length = remove_duplicates(nums)
+print(length)  # Output: 4
+print(nums[:length])  # Output: [1, 2, 3, 4]
+
+#time complexity: O(n)
+#space complexity: O(1)
